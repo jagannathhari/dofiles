1f6baad8a70d8c6a8e292c6af0a0553a677daabf
Jagannath Hari <jagannathhari17@gmail.com>
2025-04-22 11:13:30 +0530

    Ensure install

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -1,15 +1,33 @@
-  return { "nvim-treesitter/nvim-treesitter",
-  event = {"BufReadPost","BufNewFile"},
-  config=function()
-    require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-    -- enable = false,
-    additional_vim_regex_highlighting = false,
-  },
-indent = {
-    enable = true
-  }
+return
+{
+    "nvim-treesitter/nvim-treesitter",
+        event = {"BufReadPost", "BufNewFile"},
+        config = function() require 'nvim-treesitter.configs'.setup
+    {
+        highlight =
+            {
+                enable = true,
+                --enable = false,
+                additional_vim_regex_highlighting = false,
+            },
+        indent = {
+            enable = true
+        },
+      ensure_installed = {
+        "json",
+        "c",
+        "javascript",
+        "markdown",
+        "markdown_inline",
+        "comment",
+        "python",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "query",
+      },
+    }
+    end
 }
-end
-  }
